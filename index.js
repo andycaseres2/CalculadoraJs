c12b204d2798c79c2704b5644d8490cb55b19bbd
andy <andycaseres2@gmail.com>
2021-06-09 08:32:42 -0500

    listo

---
+++ index.js
@@ -0,0 +1,14 @@
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const botonesNumeros = document.querySelectorAll(".numeros");
+const botonesOperador = document.querySelectorAll(".operador");
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach((boton) => {
+  boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperador.forEach((boton) => {
+  boton.addEventListener("click", () => display.computar(boton.value));
+});
